e84749a6d9fb7004cdc6a3dcc03afe23bf61b90c
asandovala <alonso.sand2@gmail.com>
2016-09-12 22:26:23 -0300

    Agregué los archivos de la branch para integrarlos al proyecto principal

---
+++ Assets/BotEditor/Scripts/AddParts.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AddParts : MonoBehaviour {
+
+  private bool partSelected = false;
+  private GameObject part = null;
+  private string currentType = null;
+  private Dictionary<string, PrimitiveType> typeObjects; 
+
+	// Use this for initialization
+	void Start () {
+    typeObjects = new Dictionary<string, PrimitiveType>();
+    typeObjects["cube"] = PrimitiveType.Cube;
+	  typeObjects["cylinder"] = PrimitiveType.Cylinder;
+	}
+
+  public void selectPart (string type) {
+    if (partSelected) {
+      return;
+    }
+
+    if (currentType != null) {
+      if (type == currentType) {
+        return;
+      }
+    }
+
+    part = GameObject.CreatePrimitive(typeObjects[type]);
+    partSelected = true;
+    currentType = type;
+  }
+
+  void unSelectPart () {
+    if (!partSelected) {
+      return;
+    }
+
+    Destroy(part);
+    partSelected = false;
+    currentType = null;
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	   if (Input.GetMouseButtonUp(1)) {
+        unSelectPart(); 
+     }
+
+     if (partSelected) {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 10f; 
+        part.transform.position = Camera.main.ScreenToWorldPoint(temp); 
+     }
+	}
+}
---
+++ Assets/BotEditor/Scripts/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+  public GameObject target = null;
+  public int moveFactor = 150;
+
+  private KeyCode keyLeft = KeyCode.LeftArrow;
+  private KeyCode keyRight = KeyCode.RightArrow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (target != null) { 
+	   if (Input.GetKey(keyLeft)) {
+        transform.LookAt(target.transform);
+        transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * moveFactor);
+      } else if (Input.GetKey(keyRight)) {
+        transform.LookAt(target.transform);
+        transform.RotateAround(target.transform.position, Vector3.down, Time.deltaTime * moveFactor);
+      }
+    }
+	}
+}
