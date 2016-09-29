62d8c4ae66ace016221fc7659644ee8b78bd2527
Yerson Andres Escobar Maturana <yescobar@alumnos.inf.utfsm.cl>
2016-09-29 16:51:21 -0300

    Agregados el ariete y la sierra de los bots, trabajando en la bola

---
+++ Assets/Scripts/AtaqueAriete.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtaqueAriete : MonoBehaviour {
+
+	public float fuerza;
+	public float pushing;
+	public bool isPushing;
+	private Rigidbody collRigidBody;
+	private GameObject terrain;
+	private Animation animation;
+
+	// Use this for initialization
+	void Start () {
+		isPushing = false;
+		pushing = 1F / 12F;
+		terrain = GameObject.Find ("Terrain");
+		animation = this.gameObject.GetComponent<Animation> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.L)) {
+			animation.Play("ramAttack");
+			isPushing = true;
+		}
+		if (isPushing) {
+			pushing -= Time.deltaTime;
+			if (pushing < 0) {
+				pushing = 1F / 12F;
+				isPushing = false;
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision coll){
+		collRigidBody = coll.gameObject.GetComponent<Rigidbody> ();
+		//Debug.Log (collRigidBody.GetType());
+		//Debug.Log (coll.gameObject.name);
+		if (isPushing && pushing >= 0 && coll.gameObject.name != terrain.name) {
+			collRigidBody.AddForce (-this.gameObject.transform.up * fuerza, ForceMode.Impulse);
+		}
+	}
+
+}
---
+++ Assets/Scripts/AtaqueSierra.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtaqueSierra : MonoBehaviour {
+
+	public int frecuenciaRotacion;
+	public int fuerza;
+	private Rigidbody collRigidBody;
+	private GameObject terrain;
+
+	void Start () {
+		terrain = GameObject.Find ("Terrain");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, frecuenciaRotacion * Time.deltaTime, 0);
+	}
+
+	void OnCollisionEnter (Collision coll){
+		collRigidBody = coll.gameObject.GetComponent<Rigidbody> ();
+		if (coll.gameObject.name != terrain.name) {
+			collRigidBody.AddForce (-this.gameObject.transform.forward * fuerza, ForceMode.Impulse);
+		}
+	}
+}
