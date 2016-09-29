9f65c64ef4cddf86674fa40530844c5ed2b4e057
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-09-29 01:51:54 -0300

    Borra basura del proyecto, crea cierras, mejora mapa, las cierras matan al jugador, ambos jugadores pueden moverse bien

---
+++ Assets/Scripts/AllwaysSpin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AllwaysSpin : MonoBehaviour {
+
+	public float speed = 3;
+
+	void Update () {
+		transform.Rotate (0,speed,0);
+	}
+}
---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayer : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Bot 1" || col.gameObject.name == "Bot 2")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
