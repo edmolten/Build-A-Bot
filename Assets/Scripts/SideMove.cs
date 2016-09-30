f46d1a101c6861deed1cbb90aa0d31f60cc713a3
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-09-30 01:48:49 -0300

    saws en movimiento, rampas

---
+++ Assets/Scripts/SideMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SideMove : MonoBehaviour {
+
+	public float velocity;
+	float direction = 1;
+	Vector3 p;
+
+	void Update () {
+		p = transform.position;
+		transform.position = new Vector3(p.x,p.y,p.z + direction * velocity);
+		if (transform.position.z <= -32 || transform.position.z >= 2) {
+			direction *= -1;
+		}
+	}
+}
