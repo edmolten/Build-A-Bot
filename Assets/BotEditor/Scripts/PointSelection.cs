0e755c2d1b26669c824414fe9ee4b1184c022f2a
asandovala <alonso.sand2@gmail.com>
2016-10-01 18:04:43 -0300

    Branch de edición del robot

---
+++ Assets/BotEditor/Scripts/PointSelection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointSelection : MonoBehaviour {
+	
+    public AssemblerPiece assembler = null;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown(0)){
+			assembler.pointToJoin = transform.gameObject;
+			assembler.jointPartToPoint ();
+		}
+	}
+		
+}
