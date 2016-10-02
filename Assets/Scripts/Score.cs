162b0afdab2d9fc9786b03bf872582c90ccc88a2
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-10-02 01:14:05 -0300

    mas color, probando luces y fondo movil, mostrando puntaje
    issue: doble puntuacion por colision de ruedas + cuerpo

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public int count;
+
+	void Start () {
+		count = 0;
+	}
+
+	void Update () {
+		scoreText.text = "Score : " + count.ToString ();
+	}
+}
