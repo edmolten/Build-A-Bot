0101e4f502589124d3b0151bdf6cbb8814af5145
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-10-02 21:45:14 -0300

    you lose y you win

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using UnityEditor.VersionControl;
 using System.Security.Cryptography;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class KillPlayer : MonoBehaviour {
+
+	private int winCount = 10;
 
 	void OnCollisionEnter (Collision col){
 		GameObject bot;
@@ -13,12 +16,24 @@ public class KillPlayer : MonoBehaviour {
 			bot = GameObject.Find ("Bot 2");
 			scoreScript = bot.GetComponent<Score> ();
 			scoreScript.count++;
+			if (scoreScript.count == winCount) {
+				Text t = (Text) GameObject.Find ("Match Result 2").GetComponent<Text>();
+				t.text = "You Win!";
+				t = (Text) GameObject.Find ("Match Result 1").GetComponent<Text>();
+				t.text = "You Lose!";
+			}
 			respawn (col.gameObject);
 		}
 		else if (col.gameObject.name == "Bot 2") {
 			bot = GameObject.Find ("Bot 1");
 			scoreScript = bot.GetComponent<Score> ();
 			scoreScript.count++;
+			if (scoreScript.count == winCount) {
+				Text t = (Text) GameObject.Find ("Match Result 2").GetComponent<Text>();
+				t.text = "You Lose!";
+				t = (Text) GameObject.Find ("Match Result 1").GetComponent<Text>();
+				t.text = "You Win!";
+			}
 			respawn (col.gameObject);
 		}
 	}
