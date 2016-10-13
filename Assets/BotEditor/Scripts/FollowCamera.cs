6304a70cc2650a0b372e2b63a8efee8f24ad9c21
asandovala <alonso.sand2@gmail.com>
2016-10-13 12:44:29 -0300

    Posicionamiento del vehículo al iniciar.

--- Assets/BotEditor/Scripts/FollowCamera.cs
+++ Assets/BotEditor/Scripts/FollowCamera.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class FollowCamera : MonoBehaviour {
 
-  public int moveFactor = 150;
+	public int moveFactor = 150;
+	public int controlType = 0; //0 for letters and 1 for Arrows
 
-  private KeyCode keyLeft = KeyCode.LeftArrow;
-  private KeyCode keyRight = KeyCode.RightArrow;
+	private KeyCode keyA = KeyCode.A;
+  	private KeyCode keyD = KeyCode.D;
+	private KeyCode keyLeft = KeyCode.LeftArrow;
+	private KeyCode keyRight = KeyCode.RightArrow;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +18,18 @@ public class FollowCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-  
-	   if (Input.GetKey(keyLeft)) {
-        transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * moveFactor);
-      } else if (Input.GetKey(keyRight)) {
-        transform.RotateAround(transform.position, Vector3.down, Time.deltaTime * moveFactor);
-      }
+		if (controlType == 0) {	
+			if (Input.GetKey (keyLeft)) {
+				transform.RotateAround (transform.position, Vector3.up, Time.deltaTime * moveFactor);
+			} else if (Input.GetKey (keyRight)) {
+				transform.RotateAround (transform.position, Vector3.down, Time.deltaTime * moveFactor);
+			}
+		} else if (controlType == 1) {
+			if (Input.GetKey (keyA)) {
+				transform.RotateAround (transform.position, Vector3.up, Time.deltaTime * moveFactor);
+			} else if (Input.GetKey (keyD)) {
+				transform.RotateAround (transform.position, Vector3.down, Time.deltaTime * moveFactor);
+			}
+		}
 	}
 }
